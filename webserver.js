79d786577caf67afe3a427d137643c7a0f2fb0c3
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-09-17 12:52:15 -0600

    updated server to log output

--- webserver.js
+++ webserver.js
@@ -1,5 +1,6 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 
 http.listen(8080); //listen to port 8080
 
@@ -14,3 +15,13 @@ function handler (req, res) { //create server
     return res.end();
   });
 }
+
+io.sockets.on('connection', function (socket) {// WebSocket Connection
+  var lightvalue = 0; //static variable for current status
+  socket.on('light', function(data) { //get light switch status from client
+    lightvalue = data;
+    if (lightvalue) {
+      console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
+    }
+  });
+});
