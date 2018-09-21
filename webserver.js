f7cbdc9e8deb51550c60659cd08cc6571e7a969f
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-09-20 22:41:38 -0600

    retesting

--- webserver.js
+++ webserver.js
@@ -7,7 +7,7 @@ const LED = new Gpio(4, 'out'); // gpio 4 as out
 http.listen(8080 || process.env.PORT); //listen to port 8080
 
 function handler (req, res) { //create server
-  fs.readFile('https://pocket-lockit.firebaseapp.com/lock.html', function(err, data) { //read file index.html in public folder
+  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
       return res.end("404 Not Found");
