73a8a1cfbc215ba282fc64dd940a067915706fa0
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-09-21 15:10:51 -0600

    back to other code

--- webserver.js
+++ webserver.js
@@ -1,60 +1,60 @@
-'use strict';
+// 'use strict';
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+// const express = require('express');
+// const socketIO = require('socket.io');
+// const path = require('path');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join('https://pocket-lockit.firebaseapp.com/lock.html');
+// const PORT = process.env.PORT || 3000;
+// const INDEX = path.join('https://pocket-lockit.firebaseapp.com/lock.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX) )
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server);
+// const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
-
-
-// var http = require('http').createServer(handler); //require http server, and create server with function handler()
-// var fs = require('fs'); //require filesystem module
-// var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
-// const Gpio = require('onoff').Gpio;
-// const LED = new Gpio(4, 'out'); // gpio 4 as out
-
-// http.listen(8080 || process.env.PORT); //listen to port 8080
-
-// function handler (req, res) { //create server
-//   fs.readFile('https://pocket-lockit.firebaseapp.com/lock.html', function(err, data) { //read file index.html in public folder
-//     if (err) {
-//       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-//       return res.end("404 Not Found");
-//     } 
-//     res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-//     res.write(data); //write data from index.html
-//     return res.end();
-//   });
-// }
-
-// io.sockets.on('connection', function (socket) {// WebSocket Connection
-//   var lightvalue = 0; //static variable for current status
-//   socket.on('light', function(data) { //get light switch status from client
-//     lightvalue = data;
-//     if (lightvalue) {
-//     LED.writeSync(1); // make it 1 (on)
-//     console.log("on")
-//     }
-//     else {
-//       LED.writeSync(0); // make it 0 (off)
-//       console.log("working!")}
-//   });
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+//   socket.on('disconnect', () => console.log('Client disconnected'));
 // });
+
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+
+
+var http = require('http').createServer(handler); //require http server, and create server with function handler()
+var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+const Gpio = require('onoff').Gpio;
+const LED = new Gpio(4, 'out'); // gpio 4 as out
+
+http.listen(8080 || process.env.PORT); //listen to port 8080
+
+function handler (req, res) { //create server
+  fs.readFile('https://pocket-lockit.firebaseapp.com/lock.html', function(err, data) { //read file index.html in public folder
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+    res.write(data); //write data from index.html
+    return res.end();
+  });
+}
+
+io.sockets.on('connection', function (socket) {// WebSocket Connection
+  var lightvalue = 0; //static variable for current status
+  socket.on('light', function(data) { //get light switch status from client
+    lightvalue = data;
+    if (lightvalue) {
+    LED.writeSync(1); // make it 1 (on)
+    console.log("on")
+    }
+    else {
+      LED.writeSync(0); // make it 0 (off)
+      console.log("working!")}
+  });
+});
 
 
  
